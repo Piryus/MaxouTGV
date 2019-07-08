e06d048cdf1e4eab11d68e45a0817f9dbd63201d
Emile <legendre.emile.27@gmail.com>
2019-07-08 11:46:43 +0200

    Add a search bar and a date bar to the explore page

--- src/Explore.js
+++ src/Explore.js
@@ -1,10 +1,44 @@
 import React, {Component} from 'react';
-import { Text } from 'react-native';
+import { View, Text, TextInput, StyleSheet } from 'react-native';
+import { Button } from 'react-native-elements';
+import Icon from "./components/Icon";
+import SearchBar from "./components/SearchBar";
+import DateBar from "./components/DateBar";
 
 export default class HomeScreen extends Component {
   render() {
     return (
-        <Text>Page explorer</Text>
+        <View style={styles.layout}>
+         <View style={styles.searchBox}>
+             <SearchBar />
+             <DateBar />
+          </View>
+          <View style={styles.resultBox}>
+            <Text>List goes here</Text>
+          </View>
+        </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  layout : {
+    flex: 1,
+  },
+  searchBox : {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'flex-start',
+    alignItems: 'stretch',
+  },
+  resultBox : {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+  },
+  departInput: {
+    fontSize: 24,
+    margin: 50,
+  },
+});
