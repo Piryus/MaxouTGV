507990167b17bf575f5054c0cc5e63003ca5e6b6
Emile <legendre.emile.27@gmail.com>
2019-07-10 11:08:11 +0200

    Add a placeholder image for the destination tiles

--- src/components/DestTile.js
+++ src/components/DestTile.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, ImageBackground } from 'react-native';
 import theme from '../../theme';
 import Icon from "./Icon";
 
 export default class DestTile extends Component {
     render() {
         return(
-            <View style={[styles.tile, this.props.style]}>
-                <Text style={styles.trip}>{this.props.origin} -> {this.props.destination}</Text>
+            <ImageBackground source={require('../../assets/lyon.jpg')} style={[styles.tile, this.props.style]}>
+                <Text style={styles.trip}>{this.props.origin} → {this.props.destination}</Text>
                 <Text style={styles.duration}><Icon name={'stopwatch'} size={15} /> {this.props.duration}</Text>
-                <Text style={styles.hours}><Icon name={'clock'} size={15} /> {this.props.departure} -> 15h08</Text>
-            </View>
+                <Text style={styles.hours}><Icon name={'clock'} size={15} /> {this.props.departure} - 15h08</Text>
+            </ImageBackground>
         );
     }
 }
@@ -27,14 +27,20 @@ const styles = StyleSheet.create({
     trip: {
         top: 38,
         fontSize: 25,
+        color: 'white',
+        fontWeight: '600',
     },
     duration: {
         position: 'absolute',
         padding: 5,
+        color: 'white',
+        fontWeight: '600',
     },
     hours: {
         position: 'absolute',
         padding: 5,
         alignSelf: 'flex-end',
+        color: 'white',
+        fontWeight: '900',
     }
 });
