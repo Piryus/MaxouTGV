3252e7c17c7035de3146d61b77f69136d065d766
Emile <legendre.emile.27@gmail.com>
2019-07-11 16:23:19 +0200

    Implement input text and cities list

---
+++ src/components/CityListing.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { Text, View, StyleSheet } from "react-native";
+import Icon from "./Icon";
+
+export default class CityListing extends Component {
+    render() {
+        return(
+            <View style={styles.cityListing}>
+                <Icon name={'pin'} size={15} style={styles.cityPin}/>
+                <Text style={styles.cityName}>{this.props.cityName}</Text>
+            </View>
+        )
+    }
+};
+
+const styles = StyleSheet.create({
+    cityListing: {
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    cityPin: {
+        marginRight: 15,
+        color: 'black',
+    },
+    cityName: {
+        fontSize: 17,
+        color: 'black',
+    },
+});
--- src/screens/Town.js
+++ src/screens/Town.js
@@ -1,26 +1,75 @@
 import React, {Component} from 'react';
-import {Text, Button, TextInput} from 'react-native';
+import { TextInput, StyleSheet, View } from 'react-native';
+import theme from '../../theme'
+import CityListing from "../components/CityListing";
 
 export default class TownScreen extends Component<Props> {
     constructor(props) {
         super(props);
+        this.state = {
+            cityText: ''
+        };
     }
-
-    state = {
-        text: 'D\'où souhaitez-vous partir ?'
-    };
-
-    static navigationOptions =  {
-        headerTitle: (
-        <TextInput/>
-        ),
-    };
 
     render() {
         return (
-            <Text>Page de sélection de la ville de départ</Text>
+            <View>
+                <View style={styles.inputArea}>
+                    <TextInput placeholder="D'où souhaitez-vous partir ?"
+                               value={this.state.cityText}
+                               onChangeText={(cityText) => this.setState({cityText})}
+                               style={styles.inputText}
+                    />
+                </View>
+                <View style={styles.citiesArea}>
+                    <CityListing cityName={'Marseille'} />
+                    <View style={styles.separator}/>
+                    <CityListing cityName={'Lyon'} />
+                    <View style={styles.separator}/>
+                    <CityListing cityName={'Paris'} />
+                    <View style={styles.separator}/>
+                    <CityListing cityName={'Tourcoing'} />
+                </View>
+            </View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    inputArea:  {
+        backgroundColor: 'white',
+        height: 80,
+        marginBottom: 20,
+
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'stretch',
+
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 4,
+        },
+        shadowOpacity: 0.30,
+        shadowRadius: 4.65,
+
+        elevation: 8,
+    },
+    inputText: {
+        fontSize: 24,
+        fontWeight: '600',
+        marginHorizontal: 20,
+    },
+    citiesArea: {
+        flexDirection: 'column',
+        marginHorizontal: 30,
+    },
+    separator: {
+        marginVertical: 15,
+        width: '100%',
+        borderBottomColor: theme.LIGHT_GRAY,
+        borderBottomWidth: StyleSheet.hairlineWidth,
+    }
+});
 
 
