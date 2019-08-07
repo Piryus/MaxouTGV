7be18231dd1c00b444a89b0d35971aee78bb8d33
Emile <legendre.emile.27@gmail.com>
2019-08-07 21:25:40 +0200

    Change theme colors

--- theme.js
+++ theme.js
@@ -1,6 +1,10 @@
 export default {
     LIGHT_GRAY: '#e0e0e0',
     MEDIUM_GRAY: '#9e9e9e',
-    PRIMARY_COLOR: '#2196f3',
-    SECONDARY_COLOR: '#03a9f4',
+    PRIMARY_COLOR: '#03a9f4',
+    PRIMARY_COLOR_LIGHT: '#67daff',
+    PRIMARY_COLOR_DARK: '#007ac1',
+    SECONDARY_COLOR: '#ffab00',
+    SECONDARY_COLOR_LIGHT: '#ffdd4b',
+    SECONDARY_COLOR_DARK: '#c67c00',
 }
