385efe2b5858253056540cf9f6d0fbbf2dd4ea13
Emile <legendre.emile.27@gmail.com>
2019-08-12 22:39:50 +0200

    Update the city screen with a more readable date format

--- src/screens/City.js
+++ src/screens/City.js
@@ -26,7 +26,7 @@ class CityScreen extends Component {
         const title =
             <View style={headerStyles.headerTitleLayout}>
                 <Text style={headerStyles.headerTitleTripText}>{departure} <Icon name='arrow-forward' size={15}/> {destination}</Text>
-                <Text style={headerStyles.headerDate}>{date.toString()}</Text>
+                <Text style={headerStyles.headerDate}>{this.formatDate(date)}</Text>
             </View>;
         return ({
             headerTitle: title,
@@ -46,8 +46,82 @@ class CityScreen extends Component {
         const destination = this.props.navigation.getParam('destination', '???');
         const date = this.props.navigation.getParam('date');
         return (
+
             <SearchResultsFares departure={departure} destination={destination} date={date} />
         );
+    }
+
+    static formatDate(date) {
+        if (date === '') {
+            return ''
+        }
+
+        let formattedDate;
+        switch (date.getDay()) {
+            case 0:
+                formattedDate = 'Dimanche ';
+                break;
+            case 1:
+                formattedDate = 'Lundi ';
+                break;
+            case 2:
+                formattedDate = 'Mardi ';
+                break;
+            case 3:
+                formattedDate = 'Mercredi ';
+                break;
+            case 4:
+                formattedDate = 'Jeudi ';
+                break;
+            case 5:
+                formattedDate = 'Vendredi ';
+                break;
+            case 6:
+                formattedDate = 'Samedi ';
+                break;
+
+        }
+        formattedDate += date.getDate();
+        formattedDate += ' ';
+        switch (date.getMonth()) {
+            case 0:
+                formattedDate += 'janvier';
+                break;
+            case 1:
+                formattedDate += 'février';
+                break;
+            case 2:
+                formattedDate += 'mars';
+                break;
+            case 3:
+                formattedDate += 'avril';
+                break;
+            case 4:
+                formattedDate += 'mai';
+                break;
+            case 5:
+                formattedDate += 'juin';
+                break;
+            case 6:
+                formattedDate += 'juillet';
+                break;
+            case 7:
+                formattedDate += 'août';
+                break;
+            case 8:
+                formattedDate += 'septembre';
+                break;
+            case 9:
+                formattedDate += 'octobre';
+                break;
+            case 10:
+                formattedDate += 'novembre';
+                break;
+            case 11:
+                formattedDate += 'décembre';
+                break;
+        }
+        return formattedDate
     }
 }
 
