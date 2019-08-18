c430a7d03c38d2ad3d0bc1eb7e1b0e29c15bc3fd
Emile <legendre.emile.27@gmail.com>
2019-08-18 10:44:24 +0200

    Update the theme colors

--- theme.js
+++ theme.js
@@ -1,9 +1,9 @@
 export default {
     LIGHT_GRAY: '#e0e0e0',
     MEDIUM_GRAY: '#9e9e9e',
-    PRIMARY_COLOR: '#03a9f4',
-    PRIMARY_COLOR_LIGHT: '#67daff',
-    PRIMARY_COLOR_DARK: '#007ac1',
+    PRIMARY_COLOR: '#0bb7db',
+    PRIMARY_COLOR_LIGHT: '#65e9ff',
+    PRIMARY_COLOR_DARK: '#0087a9',
     SECONDARY_COLOR: '#ffab00',
     SECONDARY_COLOR_LIGHT: '#ffdd4b',
     SECONDARY_COLOR_DARK: '#c67c00',
